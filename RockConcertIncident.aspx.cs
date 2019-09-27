fcf252fa04820cc5a348eaa5723e24ddc8975790
JoshuaSilveira <joshua.silveira92@gmail.com>
2019-09-27 12:56:03 -0400

    final commit

--- RockConcertIncident.aspx.cs
+++ RockConcertIncident.aspx.cs
@@ -11,7 +11,37 @@ namespace n0140730_assignment_1b
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Page.IsPostBack)
+            {
+                String reporterName = reporter_name.Text;
+                String reporterPhone = reporter_phone.Text;
+                int affectedCount = Convert.ToInt32(affected_count.Text);
+                String incidentDate = incident_date.Text;
+                String incidentTime = incident_time.Text;
+                String incidentDescription = incident_description_textarea.Text;
 
+                confirmbox.InnerHtml = "Thank you for filling out an incident report: <br>";
+
+                confirmbox.InnerHtml += "Reporter Name: " + reporterName + "<br>";
+                confirmbox.InnerHtml += "Reporter Phone number: " + reporterPhone + "<br>";
+                confirmbox.InnerHtml += "Affected Count: " + affectedCount + "<br>";
+                confirmbox.InnerHtml += "Incident occured on: " + incidentDate + "<br>";
+                confirmbox.InnerHtml += "At approx.: " + incidentTime + "<br>";
+                confirmbox.InnerHtml += "Decription: " + incidentDescription + "<br>";
+
+                if (incidentDescription.ToLower().Contains("urgent"))
+                {
+                    confirmbox.InnerHtml += ("Urgent Attention Required");
+                }
+                else if (incidentDescription.ToLower().Contains("resolved"))
+                {
+                    confirmbox.InnerHtml += ("Incident Resolved");
+                }
+                else
+                {
+                    confirmbox.InnerHtml += ("Pending Investigation");
+                }
+            }
         }
     }
 }
